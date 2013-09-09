212fcda3620b81eb37d021bf78c83103100045ac
Edwin Chong <echong@gmail.com>
2013-09-10 03:20:22 +0800

    Get photos with specified tag and display with ID and url

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @instagram = Instagram.user_recent_media("23472277", {:count => 5})
+    # @instagram = Instagram.user_recent_media("23472277", {:count => 5})
+    @instagram = Instagram.tag_recent_media('awesome')
   end
 end
