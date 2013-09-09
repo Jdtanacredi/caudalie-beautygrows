5fd5b8e66455748b5fcf99f3a28a16ed4dffc385
Edwin Chong <echong@gmail.com>
2013-09-10 02:01:31 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CaudalieBeautygrows::Application.config.secret_token = '7c6d03bee1660825017e198238c22b6273bda44bdfeacbb192df03e141356ba0308af24e6f444688d5025e4b91395964e08d8cc8412e84c204d400f0d8f6cc88'
