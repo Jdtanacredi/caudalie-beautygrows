43c41eefc714edd22efbf5268bde9fd32e8176cc
Edwin Chong <echong@gmail.com>
2013-09-27 07:04:57 +0800

    Public gallery

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 class HomeController < ApplicationController
   def index
     # @instagram = Instagram.user_recent_media("23472277", {:count => 5})
-    @instagram = Instagram.tag_recent_media('awesome', {count: 30})
+    # @instagram = Instagram.tag_recent_media('awesome', {count: 30})
+    @photos = Photo.last(43).reverse
   end
 end
