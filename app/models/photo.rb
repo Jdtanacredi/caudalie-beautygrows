9b157497753da409d5df82cd56148c65e7b88e71
Edwin Chong <echong@gmail.com>
2013-10-07 01:19:27 +0800

    Update attr_accessible on Photo

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,5 +1,6 @@
 class Photo < ActiveRecord::Base
   resourcify
 
-  attr_accessible :approved, :instagram_id, :standard_url, :thumb_url
+  attr_accessible :approved, :instagram_id, :standard_url, :thumb_url, 
+    :caption_text, :user_username, :user_full_name, :user_profile_picture
 end
